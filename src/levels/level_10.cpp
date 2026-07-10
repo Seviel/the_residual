639047ec2595745d75bc2f2a49ed439f29a1cc64
Filip Vasiljevic <fvasiljevic@gmail.com>
2026-07-10 19:02:08 +0200

    Construct lvl 10

--- src/levels/level_10.cpp
+++ src/levels/level_10.cpp
@@ -16,9 +16,9 @@
 
 void Level_10::create_level()
 {
-    setup_player(Vector2f{100.0F, 100.0F});
+    setup_player(Vector2f{140.0F, 300.0F});
     clear_com_box();
-    camera_.set_borders({-2000.0F, -2000.0F}, {2000.0F, 2000.0F});
+    camera_.set_borders({0.0F, 0.0F}, {2000.0F, 0.0F});
 }
 
 void Level_10::update_level(double delta_time)
@@ -44,8 +44,18 @@ void Level_10::draw_level(double delta_time)
 {
     player_.draw(delta_time);
     plat_1_.draw();
+    plat_2_.draw();
+    plat_3_.draw();
+    plat_4_.draw();
+    plat_5_.draw();
+    plat_6_.draw();
+    plat_7_.draw();
+    plat_8_.draw();
     wall_1_.draw();
     wall_2_.draw();
+    wall_3_.draw();
+    wall_4_.draw();
+    wall_5_.draw();
     portal_.draw(delta_time);
     draw_com_box();
 }
--- src/levels/level_10.h
+++ src/levels/level_10.h
@@ -41,17 +41,31 @@ class Level_10 : public PlayableLevel
     std::unique_ptr<rinvid::Screen> restart_level() const override;
 
 
-    Platform plat_1_{{0.0F, 700.0F}, 1600.0F, 100.0F};
-    Platform wall_1_{{-100.0F, 0.0F}, 100.0F, 800.0F};
-    Platform wall_2_{{1600.0F, 0.0F}, 100.0F, 800.0F};
-    std::vector<Object*> platforms_{&plat_1_, &wall_1_, &wall_2_};
+    Platform plat_1_{{100.0F, 500.0F}, 450.0F, 100.0F};
+    Platform plat_2_{{600.0F, 420.0F}, 250.0F, 50.0F};
+    Platform plat_3_{{900.0F, 340.0F}, 250.0F, 50.0F};
+    Platform plat_4_{{1210.0F, 430.0F}, 280.0F, 50.0F};
+    Platform plat_5_{{1550.0F, 350.0F}, 230.0F, 50.0F};
+    Platform plat_6_{{1840.0F, 450.0F}, 240.0F, 50.0F};
+    Platform plat_7_{{2140.0F, 360.0F}, 240.0F, 50.0F};
+    Platform plat_8_{{2410.0F, 250.0F}, 290.0F, 50.0F};
 
-    TextTrigger op_trig_1_{GameText::LVL_10_OP_1, Vector2f{75.0F, 75.0F}, 200, 200,
+    Platform wall_1_{{0.0F, 0.0F}, 100.0F, 600.0F};
+    Platform wall_2_{{2700.0F, 0.0F}, 100.0F, 600.0F};
+    Platform wall_3_{{760.0F, 0.0F}, 50.0F, 260.0F};
+    Platform wall_4_{{1600.0F, 400.0F}, 50.0F, 100.0F};
+    Platform wall_5_{{2200.0F, 0.0F}, 50.0F, 230.0F};
+
+    std::vector<Object*> platforms_{&plat_1_, &plat_2_, &plat_3_, &plat_4_, &plat_5_,
+                                    &plat_6_, &plat_7_, &plat_8_, &wall_1_, &wall_2_,
+                                    &wall_3_, &wall_4_, &wall_5_};
+
+    TextTrigger op_trig_1_{GameText::LVL_10_OP_1, Vector2f{1550.0F, 150.0F}, 50, 200,
                            TextRole::Operator};
-    TextTrigger player_trig_1_{GameText::LVL_10_PLAYER_1, Vector2f{75.0F, 75.0F}, 200, 200,
+    TextTrigger player_trig_1_{GameText::LVL_10_PLAYER_1, Vector2f{300.0F, 300.0F}, 50, 200,
                                TextRole::Player};
 
-    Portal portal_{Vector2f{1350.0F, 547.0F}, Levels::level_11};
+    Portal portal_{Vector2f{2460.0F, 97.0F}, Levels::level_11};
     Camera& camera_ = RuntimeCtx::camera_;
 };
 
